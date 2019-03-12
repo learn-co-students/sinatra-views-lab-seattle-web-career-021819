d15b3946af980cf24aefb2614a3908369d3e0443
isalevine <isaacdlevine@gmail.com>
2019-03-11 17:18:29 -0700

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		date = DateTime.now
+		@date = date.strftime("%A, %B %d, %Y")
+		erb :date
+	end
+
 
 end
